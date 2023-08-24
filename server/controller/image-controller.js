1eb63bdc03404b766921a9ac36332438a4b2b297
Maqsood Ahmad <maqsoodmanzoor@gmail.com>
2023-08-25 00:17:29 +0500

    6th

--- server/controller/image-controller.js
+++ server/controller/image-controller.js
@@ -13,7 +13,6 @@ export const uploadImage = async (request, response) => {
     try {
         const file = await File.create(fileObj);
         response.status(200).json({ path: `http://localhost:${process.env.PORT}/file/${file._id}` });
-        //response.status(200).json({ path: `https://maqsoodtech-mernfilesharing.onrender.com/file/${file._id}` });
     } catch (error) {
         console.error(error.message);
         response.status(500).json({ error: error.message });
@@ -23,7 +22,6 @@ export const uploadImage = async (request, response) => {
 export const getImage = async (request, response) => {
     try {
         const file = await File.findById(request.params.fileId);
-        console.log(file)
 
         file.downloadCount++;
 
--- server/server.js
+++ server/server.js
@@ -2,9 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import router from './routes/routes.js';
 import DBConnection from './database/db.js';
-import hello from '../client/build'
 import dotenv from 'dotenv';
-const path = require('path');
+import path from 'path'
 
 dotenv.config();
 
@@ -14,15 +13,8 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// static file access 
-app.use(express.static(path.join(__dirname, '../client/build')));
 
-//routes
 app.use('/', router);
-
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
 
 const PORT = process.env.PORT || 8000;
 
